f47c84cbe87c9528b6dfb9ac2f694652f44e3f9b
박찬희 <pch0989@gmail.com>
2024-11-29 14:45:56 +0900

    cron job test

--- src/routes/api/v2/cron/updateAge/+server.ts
+++ src/routes/api/v2/cron/updateAge/+server.ts
@@ -11,14 +11,16 @@ export const POST: RequestHandler = async ({ request }) => {
 
   const seongdos = await Seongdo.find({ birth: { $regex: `${today}$` } })
 
-  Promise.all(
-    seongdos.map(async (seongdo) => {
-      await Seongdo.updateOne(
-        { _id: seongdo.id },
-        { age: getAgeFromBirth(seongdo.birth) }
-      )
-    })
-  )
+  console.log("cron job test [/api/v2/cron/updateAge]")
+
+  // Promise.all(
+  //   seongdos.map(async (seongdo) => {
+  //     await Seongdo.updateOne(
+  //       { _id: seongdo.id },
+  //       { age: getAgeFromBirth(seongdo.birth) }
+  //     )
+  //   })
+  // )
 
   if (seongdos.length > 0) {
     return json({ success: true })
